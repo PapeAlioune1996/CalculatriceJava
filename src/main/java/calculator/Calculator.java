a841326a531fa32ab43c9683dd2332c6c189749f
Papealioune96 <germainalioune96@gmail.com>
2020-11-28 02:37:19 +0100

    first

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,68 @@
+package calculator;
+
+import java.util.Arrays;
+
+public class Calculator {
+
+        private int a;
+        private int b;
+        private int som;
+        //Constructeur
+        public Calculator() {
+        }
+        ///methoode somme
+        public int sum(int a, int b)
+        {
+            int som = a+b;
+
+            return som;
+        }
+        ///methoode munis
+        public int munis(int a, int b)
+        {
+
+            som=a-b;
+            return som;
+        }
+        //methode multiplication
+        public int multiply(int a, int b)
+        {
+            return a*b;
+        }
+        //division
+        public int divide(int a,int b)
+        {
+            return a/b;
+
+        }
+        ///Minimum
+        public int min(int a, int b)
+        {
+            if(a<=b)
+                return a;
+            else
+                return b;
+        }
+        //Maximum
+        public int max(int a, int b)
+        {
+            if(a<=b)
+                return b;
+            else
+                return a;
+        }
+        //Minimum tableau
+        public int minElement(int[] list)
+        {
+            Arrays.sort(list);
+            return list[0];
+        }
+        //Maximum tableau
+        public int maxElement(int[] list)
+        {
+            Arrays.sort(list);
+            return list[list.length-1];
+        }
+
+
+}
---
+++ src/test/java/Test/CalculatorTest.java
@@ -0,0 +1,66 @@
+package Test;
+import calculator.Calculator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+public class CalculatorTest {
+
+        Calculator calc=new Calculator();
+        //test sumition
+        @Before
+        public void setUp()
+        {
+            calc=new Calculator();
+        }
+        @Test
+        public void testSum()
+        {
+
+            Assert.assertEquals(5, calc.sum(3, 2));
+        }
+        //test soustraction
+        @Test
+        public void testMunis() {
+
+            Assert.assertEquals(8, calc.munis(12, 4));
+        }
+        //test multiplication
+        @Test
+        public void testMutilply() {
+            Assert.assertEquals(15, calc.multiply(3, 5));
+        }
+        //test devide
+        @Test
+        public void testdevide() {
+
+            Assert.assertEquals(5, calc.divide(15, 3));
+        }
+        //minimum
+        @Test
+        public void testMin(){
+            int min = calc.min(10,20);
+            if (min != 10) Assert.fail();
+        }
+        //max
+        @Test
+        public void testMax(){
+            int max = calc.max(2000, -2000);
+            Assert.assertEquals(2000,max);
+        }
+        //min tab
+        @Test
+        public void testMinElement(){
+            int tab[]= {2,0,8};
+            int min = calc.minElement(tab);
+            if (min != 0) Assert.fail();
+        }
+        //max tab
+        @Test
+        public void testMaxElement(){
+            int tab[]= {2,1,5};
+            int max = calc.maxElement(tab);
+            if (max != 5) Assert.fail();
+        }
+
+
+}
